41a4677bd4a3af3227390d3536106016ded2d367
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-19 00:33:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    if self.all.detect{|song| song.name == song_name}
+    if self.find_by_name(song_name)
       song
     else
       self.create_by_name(song_name)
